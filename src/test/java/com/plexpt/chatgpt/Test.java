f3011af3b2b78f827c5a33a5eacdbb2c5437ec16
benk <mds~123456>
2023-03-21 13:14:10 +0800

    连续上下文对话

--- src/test/java/com/plexpt/chatgpt/Test.java
+++ src/test/java/com/plexpt/chatgpt/Test.java
@@ -1,15 +1,18 @@
 package com.plexpt.chatgpt;
 
+import com.alibaba.fastjson2.util.UUIDUtils;
 import com.plexpt.chatgpt.entity.billing.CreditGrantsResponse;
 import com.plexpt.chatgpt.entity.chat.ChatCompletion;
 import com.plexpt.chatgpt.entity.chat.ChatCompletionResponse;
 import com.plexpt.chatgpt.entity.chat.Message;
+import com.plexpt.chatgpt.util.ChatContextHolder;
 import com.plexpt.chatgpt.util.Proxys;
 
 import org.junit.Before;
 
 import java.net.Proxy;
 import java.util.Arrays;
+import java.util.UUID;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -25,12 +28,12 @@ public class Test {
 
     @Before
     public void before() {
-        Proxy proxy = Proxys.http("127.0.0.1", 1080);
+        // Proxy proxy = Proxys.http("127.0.0.1", 1080);
 
         chatGPT = ChatGPT.builder()
-                .apiKey("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")
+                .apiKey("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")
                 .timeout(900)
-                .proxy(proxy)
+                // .proxy(proxy)
                 .apiHost("https://api.openai.com/") //代理地址
                 .build()
                 .init();
@@ -61,4 +64,31 @@ public class Test {
         System.out.println(res);
     }
 
+
+    @org.junit.Test
+    public void chatContext() {
+        String chatId = UUID.randomUUID().toString();
+
+        Message m1 = Message.of("安静的近义词有哪些？");
+        ChatContextHolder.add(chatId, m1);
+
+        ChatCompletion chatCompletion = ChatCompletion.builder()
+                .model(ChatCompletion.Model.GPT_3_5_TURBO.getName())
+                .messages(ChatContextHolder.get(chatId))
+                .maxTokens(3000)
+                .temperature(0.9)
+                .build();
+        ChatCompletionResponse response = chatGPT.chatCompletion(chatCompletion);
+        Message res = response.getChoices().get(0).getMessage();
+        System.out.println(res);
+
+        // 接着问
+        Message m2 = Message.of("列出5个即可？");
+        ChatContextHolder.add(chatId, m2);
+        chatCompletion.setMessages(ChatContextHolder.get(chatId));
+        response = chatGPT.chatCompletion(chatCompletion);
+        res = response.getChoices().get(0).getMessage();
+        System.out.println(res);
+
+    }
 }
